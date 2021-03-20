9c35fdc81b821ad643b59b2504f940db7f48c624
NMunro <neilmunro@gmail.com>
2021-03-20 08:36:42 +0000

    Proof of concept conversion

--- src/distance/main.lisp
+++ src/distance/main.lisp
@@ -1,8 +1,8 @@
 (defpackage cl-unit.distance
   (:use :cl
    :cl-unit.distance.base
-   :cl-unit.distance.centimeter
    :cl-unit.distance.meter
+   :cl-unit.distance.centimeter
    :cl-unit.distance.kilometer)
   (:export #:centimeter
            #:make-centimeter
@@ -16,17 +16,17 @@
 (defgeneric convert (obj1 target)
   (:documentation "Converts an object of one type, to another"))
 
+(defmethod convert ((obj1 meter) (target kilometer))
+  (/ (val obj1) 1000))
+
 (let ((m (make-meter 5)))
   (val m))
 
 (let ((cm (make-centimeter 5)))
   cm)
 
-(let ((km (make-kilometer 5)))
-  (base km))
-
-(let ((km (make-kilometer 5)))
-  (convert km 'meter))
+(let ((m (make-meter 5)))
+  (convert m (make-kilometer 0)))
 
 #|
 (macroexpand-1 '(define-distance meter nil))
