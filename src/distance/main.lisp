9e761e690c8c13e727416585e8a913fcd1e10b69
NMunro <neilmunro@gmail.com>
2021-03-21 14:33:42 +0000

    Implemented the maths operators for distance

--- src/distance/main.lisp
+++ src/distance/main.lisp
@@ -4,6 +4,7 @@
    :cl-unit.distance.meter
    :cl-unit.distance.centimeter
    :cl-unit.distance.kilometer)
+  (:shadow "+" "-" "/" "*")
   (:export #:centimeter
            #:make-centimeter
            #:meter
@@ -17,7 +18,62 @@
   (:documentation "Converts an object of one type, to another"))
 
 (defmethod convert ((obj1 meter) (target kilometer))
-  (/ (val obj1) 1000))
+  (make-kilometer (/ (val obj1) 1000)))
+
+(defmethod convert ((obj1 kilometer) (target meter))
+  (make-meter (* (val obj1) 1000)))
+
+(defun + (&rest units)
+  (reduce 'add-distance (cdr units) :initial-value (car units)))
+
+(defun * (&rest units)
+  (reduce 'multiply-distance (cdr units) :initial-value (car units)))
+
+(defun - (&rest units)
+  (reduce 'subtract-distance (cdr units) :initial-value (car units)))
+
+(defun / (&rest units)
+  (reduce 'divide-distance (cdr units) :initial-value (car units)))
+
+(defgeneric add-distance (unit1 unit2)
+  (:documentation "Adds two units"))
+
+(defgeneric multiply-distance (unit1 unit2)
+  (:documentation "Multiplies two units"))
+
+(defgeneric subtract-distance (unit1 unit2)
+  (:documentation "Subtract two units"))
+
+(defgeneric divide-distance (unit1 unit2)
+  (:documentation "Divide two units"))
+
+(defmethod add-distance ((unit1 meter) (unit2 meter))
+  (make-meter (cl:+ (val unit1) (val unit2))))
+
+(defmethod multiply-distance ((unit1 meter) (unit2 meter))
+  (make-meter (cl:* (val unit1) (val unit2))))
+
+(defmethod subtract-distance ((unit1 meter) (unit2 meter))
+  (make-meter (cl:- (val unit1) (val unit2))))
+
+(defmethod divide-distance ((unit1 meter) (unit2 meter))
+  (make-meter (cl:/ (val unit1) (val unit2))))
+
+(let ((meter1 (make-meter 2))
+      (meter2 (make-meter 5)))
+    (+ meter1 meter2))
+
+(let ((meter1 (make-meter 2))
+      (meter2 (make-meter 5)))
+    (* meter1 meter2))
+
+(let ((meter1 (make-meter 2))
+      (meter2 (make-meter 5)))
+    (- meter1 meter2))
+
+(let ((meter1 (make-meter 2))
+      (meter2 (make-meter 5)))
+    (/ meter1 meter2))
 
 (let ((m (make-meter 5)))
   (val m))
@@ -27,6 +83,9 @@
 
 (let ((m (make-meter 5)))
   (convert m (make-kilometer 0)))
+
+(let ((km (make-kilometer 1)))
+  (convert km (make-meter 0)))
 
 #|
 (macroexpand-1 '(define-distance meter nil))
