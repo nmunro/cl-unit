743b0df75cf151315c29a696f7ea7903fa6dd6b7
NMunro <neilmunro@gmail.com>
2021-03-24 01:12:24 +0000

    Cleaning up distance main package

--- src/distance/main.lisp
+++ src/distance/main.lisp
@@ -3,27 +3,14 @@
    :cl-unit.distance.centimeter
    :cl-unit.distance.meter
    :cl-unit.distance.kilometer
-   :cl-unit.distance.convert
-   :cl-unit.distance.add
-   :cl-unit.distance.subtract
-   :cl-unit.distance.multiply
-   :cl-unit.distance.divide)
+   :cl-unit.distance.convert)
   (:import-from :cl-unit.distance.base :val)
-  (:shadow "+" "-" "/" "*")
-  (:export #:+
-           #:-
-           #:/
-           #:*
-           #:centimeter
+  (:export #:centimeter
            #:make-centimeter
            #:meter
            #:make-meter
            #:kilometer
            #:make-kilometer
-           #:add-distance
-           #:subtract-distance
-           #:multiply-distance
-           #:divide-distance
            #:val
            #:convert))
 (in-package :cl-unit.distance)
