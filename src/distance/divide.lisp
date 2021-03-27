9ba61612210fda3bc88b9fe192787dfaed9ca9f4
NMunro <neilmunro@gmail.com>
2021-03-27 11:48:23 +0000

    #14 Added function stubs for /

--- src/distance/divide.lisp
+++ src/distance/divide.lisp
@@ -10,8 +10,26 @@
 (defmethod divide-distance ((unit1 meter) (unit2 meter))
   (make-meter (/ (val unit1) (val unit2))))
 
+(defmethod divide-distance ((unit1 meter) (unit2 kilometer))
+  (make-meter (/ (val unit1) (* 1000 (val unit2)))))
+
+(defmethod divide-distance ((unit1 meter) (unit2 centimeter))
+  (make-meter (/ (val unit1) (val unit2))))
+
 (defmethod divide-distance ((unit1 kilometer) (unit2 kilometer))
   (make-kilometer (/ (val unit1) (val unit2))))
 
+(defmethod divide-distance ((unit1 kilometer) (unit2 meter))
+  (make-meter (/ (* 1000 (val unit1)) (val unit2))))
+
+(defmethod divide-distance ((unit1 kilometer) (unit2 centimeter))
+  (make-meter (/ (* 1000 (val unit1)) (val unit2))))
+
 (defmethod divide-distance ((unit1 centimeter) (unit2 centimeter))
+  (make-centimeter (/ (val unit1) (val unit2))))
+
+(defmethod divide-distance ((unit1 centimeter) (unit2 meter))
+  (make-centimeter (/ (val unit1) (val unit2))))
+
+(defmethod divide-distance ((unit1 centimeter) (unit2 kilometer))
   (make-centimeter (/ (val unit1) (val unit2))))
