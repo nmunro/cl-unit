328b50263ba12136c21bea72983cb5344be8d12e
NMunro <neilmunro@gmail.com>
2021-04-07 17:40:50 +0100

    #18 Added tests for >=

--- tests/centimeter.lisp
+++ tests/centimeter.lisp
@@ -61,6 +61,26 @@
   (testing "should (>= (make-centimeter 1) (make-centimeter 2)) to be false"
     (ng (cl-unit:>= (cl-unit.distance:make-centimeter 1) (cl-unit.distance:make-centimeter 2)))))
 
+(deftest test-centimeter>=meter-1
+  (testing "should (>= (make-centimeter 100) (make-meter 1)) to be false"
+    (ok (cl-unit:>= (cl-unit.distance:make-centimeter 100) (cl-unit.distance:make-meter 1)))))
+
+(deftest test-centimeter>=meter-2
+  (testing "should (>= (make-centimeter 101) (make-meter 1)) to be false"
+    (ok (cl-unit:>= (cl-unit.distance:make-centimeter 101) (cl-unit.distance:make-meter 1)))))
+
+(deftest test-centimeter>=kilometer-1
+  (testing "should (>= (make-centimeter 1) (make-kilometer 1)) to be false"
+    (ng (cl-unit:>= (cl-unit.distance:make-centimeter 1) (cl-unit.distance:make-kilometer 1)))))
+
+(deftest test-centimeter>=kilometer-2
+  (testing "should (>= (make-centimeter 100000) (make-kilometer 1)) to be true"
+    (ok (cl-unit:>= (cl-unit.distance:make-centimeter 100000) (cl-unit.distance:make-kilometer 1)))))
+
+(deftest test-centimeter>=kilometer-3
+  (testing "should (>= (make-centimeter 100001) (make-kilometer 1)) to be true"
+    (ok (cl-unit:>= (cl-unit.distance:make-centimeter 100001) (cl-unit.distance:make-kilometer 1)))))
+
 (deftest test-centimeter<centimeter-1
   (testing "should (< (make-meter 1) (make-meter 2)) to be true"
     (ok (cl-unit:< (cl-unit.distance:make-centimeter 1) (cl-unit.distance:make-centimeter 2)))))
